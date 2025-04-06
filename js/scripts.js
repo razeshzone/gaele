015d5271d3c3d192d7f87b84b08602f5aed13dad
Rajesh Kr. Das <razeshzone@gmail.com>
2025-04-06 09:59:55 +0545

    changes
    fixes for index and album

--- js/scripts.js
+++ js/scripts.js
@@ -4,33 +4,44 @@ function triggerGlowImage(id) {
   $(`.island[data-id="${id}"] .glow-image`).fadeIn(150);
 }
 
-// Desktop hover
+// Desktop hover: show glow on mouseenter/leave
 $(document).on('mouseenter', '.nav-link', function () {
   if ($(window).width() >= 992) {
     const id = $(this).data('id');
     triggerGlowImage(id);
   }
 });
-
 $(document).on('mouseleave', '.nav-link', function () {
   if ($(window).width() >= 992) {
     $('.glow-image').fadeOut(100);
   }
 });
 
-// Mobile + click
+// Mobile + click (and also desktop click now)
 $(document).on('click', '.nav-link', function (e) {
-  e.preventDefault();
   const id = $(this).data('id');
   triggerGlowImage(id);
-
+  
+  // If on mobile, close the offcanvas menu.
   if ($(window).width() < 992) {
     const offcanvasEl = document.getElementById('mobileSidebar');
     const offcanvas = bootstrap.Offcanvas.getInstance(offcanvasEl);
-    offcanvas?.hide();
+    if (offcanvas) {
+      offcanvas.hide();
+    }
+  }
+  
+  // Get the href attribute of the clicked link.
+  const href = $(this).attr('href');
+  
+  // If the href is not "#" (i.e. a real URL), delay navigation slightly.
+  if (href && href !== "#") {
+    e.preventDefault();  // Prevent immediate navigation.
+    setTimeout(function () {
+      window.location.href = href;
+    }, 200); // Delay 200ms to allow the glow effect to show.
   }
 });
-
 
 //swiper js
 var swiper = new Swiper(".gaele-swiper", {
